ce3e8c370e715996cb19bb00977f5494d9da088b
CobaltKrayt <57502696+CobaltKrayt@users.noreply.github.com>
2020-11-07 21:15:51 +0200

    Day 3 actually: understood how the communication between kotlin and xml files works and implemented some actions based on user input

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -23,6 +23,7 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
@@ -32,6 +33,10 @@ class TitleFragment : Fragment() {
                 inflater, R.layout.fragment_title, container, false)
         binding.playButton.setOnClickListener (
                 Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment))
+
+        //lambda variant
+        //binding.playButton.setOnClickListener{view: View -> view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)}
+
         return binding.root
     }
 }
